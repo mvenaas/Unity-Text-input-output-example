471a5992d0c6ce23fba4ed3f1d95364fa6f2dfb9
Martin Venaas <m@martinvenaas.com>
2017-07-27 13:13:04 +1200

    Initial commit

---
+++ Assets/TextInput.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextInput : MonoBehaviour {
+	InputField input;
+	InputField.SubmitEvent se;
+	InputField.OnChangeEvent ce;
+	public Text output;
+
+	// Use this for initialization
+	void Start () {
+		input = this.GetComponent<InputField>();
+		se = new InputField.SubmitEvent();
+		se.AddListener(SubmitInput);
+
+        ce = new InputField.OnChangeEvent();
+        ce.AddListener(ChangeInput);
+
+        input.onEndEdit = se;
+        input.onValueChanged = ce;
+
+    }
+	
+	// Update is called once per frame
+	/*
+	 * void Update () {
+	
+	}
+	*/
+
+	private void SubmitInput(string arg0)
+	{
+		string currentText = output.text;
+
+        //  DO THIS LATER 
+        // CommandProcessor aCmd = new CommandProcessor();
+        // output.text = aCmd.Parse(arg0);
+        string newText = currentText + "\n" + arg0; 
+        output.text = newText;
+
+		input.text = "";
+		input.ActivateInputField();
+        
+
+
+	}
+
+	private void ChangeInput( string arg0)
+	{
+		Debug.Log(arg0);
+	}
+}
